3d5c2c8d895ca5b1c93c7b87570d1bc53da84261
Luis <estrada.villegas.17071@itsmante.edu.mx>
2021-01-27 06:42:02 -0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace NotacionesComple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.caja = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.prefijo = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel6 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.normal = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel8 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel3 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.subfijo = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(638, 537);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(589, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 26);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Poppins", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.Color.White;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(221, 39);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(175, 56);
+            this.bunifuCustomLabel1.TabIndex = 2;
+            this.bunifuCustomLabel1.Text = "Notacion";
+            // 
+            // caja
+            // 
+            this.caja.BackColor = System.Drawing.Color.White;
+            this.caja.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.caja.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.caja.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.caja.HintForeColor = System.Drawing.Color.Empty;
+            this.caja.HintText = "";
+            this.caja.isPassword = false;
+            this.caja.LineFocusedColor = System.Drawing.Color.Gold;
+            this.caja.LineIdleColor = System.Drawing.Color.Gray;
+            this.caja.LineMouseHoverColor = System.Drawing.Color.Gold;
+            this.caja.LineThickness = 3;
+            this.caja.Location = new System.Drawing.Point(116, 156);
+            this.caja.Margin = new System.Windows.Forms.Padding(4);
+            this.caja.Name = "caja";
+            this.caja.Size = new System.Drawing.Size(323, 41);
+            this.caja.TabIndex = 4;
+            this.caja.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 12;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.ForeColor = System.Drawing.Color.White;
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(121, 124);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(185, 28);
+            this.bunifuCustomLabel2.TabIndex = 5;
+            this.bunifuCustomLabel2.Text = "Ingrese una Expresion";
+            // 
+            // bunifuFlatButton1
+            // 
+            this.bunifuFlatButton1.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.bunifuFlatButton1.BackColor = System.Drawing.Color.Goldenrod;
+            this.bunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuFlatButton1.BorderRadius = 7;
+            this.bunifuFlatButton1.ButtonText = "       Convertir";
+            this.bunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuFlatButton1.DisabledColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.bunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent;
+            this.bunifuFlatButton1.Iconimage = ((System.Drawing.Image)(resources.GetObject("bunifuFlatButton1.Iconimage")));
+            this.bunifuFlatButton1.Iconimage_right = null;
+            this.bunifuFlatButton1.Iconimage_right_Selected = null;
+            this.bunifuFlatButton1.Iconimage_Selected = null;
+            this.bunifuFlatButton1.IconMarginLeft = 0;
+            this.bunifuFlatButton1.IconMarginRight = 0;
+            this.bunifuFlatButton1.IconRightVisible = true;
+            this.bunifuFlatButton1.IconRightZoom = 0D;
+            this.bunifuFlatButton1.IconVisible = true;
+            this.bunifuFlatButton1.IconZoom = 50D;
+            this.bunifuFlatButton1.IsTab = false;
+            this.bunifuFlatButton1.Location = new System.Drawing.Point(116, 213);
+            this.bunifuFlatButton1.Name = "bunifuFlatButton1";
+            this.bunifuFlatButton1.Normalcolor = System.Drawing.Color.Goldenrod;
+            this.bunifuFlatButton1.OnHovercolor = System.Drawing.Color.DarkGoldenrod;
+            this.bunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(49)))));
+            this.bunifuFlatButton1.selected = false;
+            this.bunifuFlatButton1.Size = new System.Drawing.Size(228, 48);
+            this.bunifuFlatButton1.TabIndex = 6;
+            this.bunifuFlatButton1.Text = "       Convertir";
+            this.bunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bunifuFlatButton1.Textcolor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(49)))));
+            this.bunifuFlatButton1.TextFont = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuFlatButton1.Click += new System.EventHandler(this.bunifuFlatButton1_Click);
+            // 
+            // prefijo
+            // 
+            this.prefijo.AutoSize = true;
+            this.prefijo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.prefijo.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prefijo.ForeColor = System.Drawing.Color.White;
+            this.prefijo.Location = new System.Drawing.Point(111, 392);
+            this.prefijo.Name = "prefijo";
+            this.prefijo.Size = new System.Drawing.Size(87, 28);
+            this.prefijo.TabIndex = 9;
+            this.prefijo.Text = "Expresion";
+            this.prefijo.Click += new System.EventHandler(this.bunifuCustomLabel5_Click);
+            // 
+            // bunifuCustomLabel6
+            // 
+            this.bunifuCustomLabel6.AutoSize = true;
+            this.bunifuCustomLabel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.bunifuCustomLabel6.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel6.ForeColor = System.Drawing.Color.Gold;
+            this.bunifuCustomLabel6.Location = new System.Drawing.Point(111, 291);
+            this.bunifuCustomLabel6.Name = "bunifuCustomLabel6";
+            this.bunifuCustomLabel6.Size = new System.Drawing.Size(176, 28);
+            this.bunifuCustomLabel6.TabIndex = 10;
+            this.bunifuCustomLabel6.Text = "Expresión ingresada:";
+            // 
+            // normal
+            // 
+            this.normal.AutoSize = true;
+            this.normal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.normal.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.normal.ForeColor = System.Drawing.Color.White;
+            this.normal.Location = new System.Drawing.Point(111, 319);
+            this.normal.Name = "normal";
+            this.normal.Size = new System.Drawing.Size(87, 28);
+            this.normal.TabIndex = 11;
+            this.normal.Text = "Expresion";
+            this.normal.Click += new System.EventHandler(this.bunifuCustomLabel7_Click);
+            // 
+            // bunifuCustomLabel8
+            // 
+            this.bunifuCustomLabel8.AutoSize = true;
+            this.bunifuCustomLabel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.bunifuCustomLabel8.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel8.ForeColor = System.Drawing.Color.Gold;
+            this.bunifuCustomLabel8.Location = new System.Drawing.Point(111, 364);
+            this.bunifuCustomLabel8.Name = "bunifuCustomLabel8";
+            this.bunifuCustomLabel8.Size = new System.Drawing.Size(63, 28);
+            this.bunifuCustomLabel8.TabIndex = 12;
+            this.bunifuCustomLabel8.Text = "Prefijo:";
+            this.bunifuCustomLabel8.Click += new System.EventHandler(this.bunifuCustomLabel8_Click);
+            // 
+            // bunifuCustomLabel3
+            // 
+            this.bunifuCustomLabel3.AutoSize = true;
+            this.bunifuCustomLabel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.bunifuCustomLabel3.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel3.ForeColor = System.Drawing.Color.Gold;
+            this.bunifuCustomLabel3.Location = new System.Drawing.Point(111, 440);
+            this.bunifuCustomLabel3.Name = "bunifuCustomLabel3";
+            this.bunifuCustomLabel3.Size = new System.Drawing.Size(71, 28);
+            this.bunifuCustomLabel3.TabIndex = 14;
+            this.bunifuCustomLabel3.Text = "Postfijo:";
+            // 
+            // subfijo
+            // 
+            this.subfijo.AutoSize = true;
+            this.subfijo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.subfijo.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subfijo.ForeColor = System.Drawing.Color.White;
+            this.subfijo.Location = new System.Drawing.Point(111, 468);
+            this.subfijo.Name = "subfijo";
+            this.subfijo.Size = new System.Drawing.Size(87, 28);
+            this.subfijo.TabIndex = 13;
+            this.subfijo.Text = "Expresion";
+            // 
+            // bunifuElipse2
+            // 
+            this.bunifuElipse2.ElipseRadius = 8;
+            this.bunifuElipse2.TargetControl = this.caja;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 534);
+            this.Controls.Add(this.bunifuCustomLabel3);
+            this.Controls.Add(this.subfijo);
+            this.Controls.Add(this.bunifuCustomLabel8);
+            this.Controls.Add(this.normal);
+            this.Controls.Add(this.bunifuCustomLabel6);
+            this.Controls.Add(this.prefijo);
+            this.Controls.Add(this.bunifuFlatButton1);
+            this.Controls.Add(this.bunifuCustomLabel2);
+            this.Controls.Add(this.caja);
+            this.Controls.Add(this.bunifuCustomLabel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox caja;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel3;
+        private Bunifu.Framework.UI.BunifuCustomLabel subfijo;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel8;
+        private Bunifu.Framework.UI.BunifuCustomLabel normal;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel6;
+        private Bunifu.Framework.UI.BunifuCustomLabel prefijo;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotacionesComple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Stack<String> operandos = new Stack<string>();
+        private Stack<String> operadores = new Stack<string>();
+        private Stack<String> stack_ptfx = new Stack<string>();
+        private string[] operatorsAccepted = { "+", "-", "/", "*" };
+
+        void proccessingEntry(string cadena)
+        {
+            this.prefijo.Text = this.toprefijo(cadena);
+            this.subfijo.Text = this.toPostfijo(cadena);
+        }
+
+
+        string toprefijo(string cadena)
+        {
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (cadena[i] == '(')
+                {
+                    operadores.Push(cadena[i].ToString());
+                }
+                else if (cadena[i] == ')')
+                {
+                    while (operadores.Count != 0 && operadores.Peek() != "(")
+                    {
+                        String op1 = operandos.Peek();
+                        operandos.Pop();
+
+                        String op2 = operandos.Peek();
+                        operandos.Pop();
+
+                        string op = operadores.Peek();
+                        operadores.Pop();
+
+                        String tmp = op + op2 + op1;
+                        operandos.Push(tmp);
+                    }
+                    operadores.Pop();
+                }
+                else if (!this.operatorsAccepted.Contains(cadena[i].ToString()))
+                {
+                    operandos.Push(cadena[i] + "");
+                }
+                else
+                {
+                    while (operadores.Count != 0 && getOperatorPriority(cadena[i]) <= getOperatorPriority(operadores.Peek()[0]))
+                    {
+
+                        String op1 = operandos.Pop();
+                        String op2 = operandos.Pop();
+
+                        string op = operadores.Pop();
+
+                        String tmp = op + op2 + op1;
+                        operandos.Push(tmp);
+                    }
+                    operadores.Push(cadena[i].ToString());
+                }
+            }
+
+            while (operadores.Count != 0)
+            {
+                String op1 = operandos.Peek();
+                operandos.Pop();
+
+                String op2 = operandos.Peek();
+                operandos.Pop();
+
+                string op = operadores.Peek();
+                operadores.Pop();
+
+                String tmp = op + op2 + op1;
+                operandos.Push(tmp);
+            }
+            return this.rotateEntry(operandos.Peek());
+        }
+
+        string toPostfijo(string cadena)
+        {
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (cadena[i] == '(')
+                {
+                    operadores.Push(cadena[i].ToString());
+                }
+                else if (cadena[i] == ')')
+                {
+                    while (operadores.Count != 0 && operadores.Peek() != "(")
+                    {
+                        String op1 = operandos.Peek();
+                        operandos.Pop();
+
+                        String op2 = operandos.Peek();
+                        operandos.Pop();
+
+                        string op = operadores.Peek();
+                        operadores.Pop();
+
+                        String tmp = op + op2 + op1;
+                        operandos.Push(tmp);
+                    }
+                    operadores.Pop();
+                }
+                else if (!this.operatorsAccepted.Contains(cadena[i].ToString()))
+                {
+                    operandos.Push(cadena[i] + "");
+                }
+                else
+                {
+                    while (operadores.Count != 0 && getOperatorPriority(cadena[i]) <= getOperatorPriority(operadores.Peek()[0]))
+                    {
+
+                        String op1 = operandos.Pop();
+                        String op2 = operandos.Pop();
+
+                        string op = operadores.Pop();
+
+                        String tmp = op + op2 + op1;
+                        operandos.Push(tmp);
+                    }
+                    operadores.Push(cadena[i].ToString());
+                }
+            }
+
+            while (operadores.Count != 0)
+            {
+                String op1 = operandos.Peek();
+                operandos.Pop();
+
+                String op2 = operandos.Peek();
+                operandos.Pop();
+
+                string op = operadores.Peek();
+                operadores.Pop();
+
+                String tmp = op + op2 + op1;
+                operandos.Push(tmp);
+            }
+            return this.rotateEntry(operandos.Peek());
+        }
+
+        string rotateEntry(string entry)
+        {
+            char[] cadenaComoCaracteres = entry.ToCharArray();
+            Array.Reverse(cadenaComoCaracteres);
+            return new string(cadenaComoCaracteres);
+        }
+
+        
+
+        int getOperatorPriority(char C)
+        {
+            if (C == '-' || C == '+')
+                return 1;
+            else if (C == '*' || C == '/')
+                return 2;
+            else
+                return 0;
+        }
+
+
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void bunifuCustomLabel8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomLabel5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            proccessingEntry(caja.Text);
+
+            string cadena = caja.Text;
+            normal.Text = cadena;
+        }
+
+        private void bunifuCustomLabel7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int Y = 0;
+        int X = 0;
+
+        int posY = 0;
+        int posX = 0;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+
+                posX = e.X;
+                posY = e.Y;
+            }
+            else
+            {
+
+                Left = Left + (e.X - posX);
+                Top = Top + (e.Y - posY);
+
+            }
+        }
+    }
+}
